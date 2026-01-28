b2e1e0c58609f610e9e571a45e7f864ba717edfa
Eternity1824 <131894021+Eternity1824@users.noreply.github.com>
2026-01-28 02:55:27 -0800

    cp docs and server

---
+++ server/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "com.chatflow"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("io.netty:netty-all:4.2.8.Final")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "ChatFlow"
+include("common")
+include("server")
+include("client-part1")
+include("client-part2")
