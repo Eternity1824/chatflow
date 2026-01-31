3cda58b991fe2817eab7b62699fed1551c35224b
Eternity1824 <131894021+Eternity1824@users.noreply.github.com>
2026-01-31 00:50:57 -0800

    add application plugin

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("application")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.chatflow"
@@ -7,6 +9,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass.set("com.chatflow.server.ChatServer")
 }
 
 dependencies {
@@ -26,4 +32,9 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    archiveClassifier.set("all")
+    mergeServiceFiles()
 }
